1345cdd0615b6f9500e1733de5935eced72c38fd
xRicardo <69592681+xRicardo@users.noreply.github.com>
2021-06-19 13:12:56 -0500

    five commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -81,6 +81,14 @@ Widget derecha() {
         Card(
           child: Column(
             children: [
+              modal1(),
+              modal2(),
+            ],
+          ),
+        ),
+        Card(
+          child: Column(
+            children: [
               Text("ALL", style: TextStyle(fontSize: 12)),
               vent1(),
               vent2(),
@@ -283,134 +291,112 @@ Widget vent5() {
   );
 }
 
-Widget vent1() {
+Widget modal1() {
   return Container(
-    child: Card(
-      child: Container(
-        height: 100,
-        width: 500,
-        child: Column(
+    child: Row(
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Container(
-              height: 170,
-              width: 160,
-              decoration: BoxDecoration(
-                image: DecorationImage(image: AssetImage("microondas.jpeg")),
+            Card(
+              child: Container(
+                height: 180,
+                width: 320,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      height: 70,
+                      width: 70,
+                      decoration: BoxDecoration(
+                        image:
+                            DecorationImage(image: AssetImage("labadora.jpeg")),
+                      ),
+                    ),
+                    Text(
+                      "Washer Machine",
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      "Ready Wash Stainless",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      "4.6",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Text(
-              "Microwave Oven",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
             ),
           ],
         ),
-      ),
+      ],
     ),
   );
 }
 
-Widget vent2() {
+Widget modal2() {
   return Container(
-    child: Card(
-      child: Container(
-        height: 100,
-        width: 500,
-        child: Column(
+    child: Row(
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Container(
-              height: 170,
-              width: 160,
-              decoration: BoxDecoration(
-                image: DecorationImage(image: AssetImage("batidora.jpeg")),
+            Card(
+              child: Container(
+                height: 180,
+                width: 320,
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      height: 70,
+                      width: 70,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("multi-lavadora.jpeg")),
+                      ),
+                    ),
+                    Text(
+                      "Dryer Machine",
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      "Ready Wash Wash Stanless",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      "4.6",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Text(
-              "Whisk",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
             ),
           ],
         ),
-      ),
-    ),
-  );
-}
-
-Widget vent3() {
-  return Container(
-    child: Card(
-      child: Container(
-        height: 100,
-        width: 500,
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 170,
-              width: 160,
-              decoration: BoxDecoration(
-                image:
-                    DecorationImage(image: AssetImage("estereo-equipo.jpeg")),
-              ),
-            ),
-            Text(
-              "Speakers",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-            ),
-          ],
-        ),
-      ),
-    ),
-  );
-}
-
-Widget vent4() {
-  return Container(
-    child: Card(
-      child: Container(
-        height: 100,
-        width: 500,
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 170,
-              width: 160,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("aire-acondicionador.jpeg")),
-              ),
-            ),
-            Text(
-              "Air Conditioner",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-            ),
-          ],
-        ),
-      ),
-    ),
-  );
-}
-
-Widget vent5() {
-  return Container(
-    child: Card(
-      child: Container(
-        height: 100,
-        width: 500,
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 170,
-              width: 160,
-              decoration: BoxDecoration(
-                image: DecorationImage(image: AssetImage("television.jpeg")),
-              ),
-            ),
-            Text(
-              "Television",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-            ),
-          ],
-        ),
-      ),
+      ],
     ),
   );
 }
